f36e12d6b36ee8d227cb4f7a2ff8e56f2d34df5f
Meghal <meghal.softwaredev@gmail.com>
2022-01-11 21:29:53 -0500

    Update look and feel of sender bubble

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -4,15 +4,15 @@ import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, user } = props;
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+        return message.senderId === user.id ? (
+          <SenderBubble key={message.id} text={message.text} time={time} user={user} />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Box, Typography, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -8,34 +8,50 @@ const useStyles = makeStyles(() => ({
     flexDirection: "column",
     alignItems: "flex-end"
   },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    marginLeft: 11,
+    marginTop: 6
+  },
   date: {
     fontSize: 11,
     color: "#BECCE2",
     fontWeight: "bold",
     marginBottom: 5
   },
+  bubble: {
+    marginRight: 20,
+    backgroundImage: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)",
+    borderRadius: "10px 0px 10px 10px"
+  },
   text: {
     fontSize: 14,
-    color: "#91A3C0",
+    fontWeight: "bold",
+    color: "#FFFFFF",
     letterSpacing: -0.2,
-    padding: 8,
-    fontWeight: "bold"
-  },
-  bubble: {
-    background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
+    padding: 8
   }
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { text, time, user } = props;
   return (
     <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-        <Typography className={classes.text}>{text}</Typography>
+      <Box className={classes.row}>
+        <Typography className={classes.date}>
+          {user.username} {time}
+        </Typography>
+        <Avatar alt={user.username} src={user.photoUrl} className={classes.avatar}></Avatar>
       </Box>
+        <Box className={classes.bubble}>
+          <Typography className={classes.text}>{text}</Typography>
+        </Box>
     </Box>
   );
 };
